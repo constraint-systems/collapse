2785a8da645a4fe3953b09dfd07dbab8d64b97a4
grant <grantcuster@gmail.com>
2020-03-20 12:55:41 -0400

    Deploy to gh-pages

--- _next/static/development/pages/_app.js
+++ _next/static/development/pages/_app.js
@@ -3235,7 +3235,7 @@ try {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! next-client-pages-loader?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app!./");
-module.exports = __webpack_require__(/*! /home/grant/s/cs/spiral/node_modules/next/dist/client/router.js */"./node_modules/next/dist/client/router.js");
+module.exports = __webpack_require__(/*! /home/grant/s/cs/collapse/node_modules/next/dist/client/router.js */"./node_modules/next/dist/client/router.js");
 
 
 /***/ }),
